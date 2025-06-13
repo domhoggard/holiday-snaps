19158281857da69195f333a409eb5b02a38f7130
domhoggard <domhoggard@me.com>
2025-06-13 10:21:06 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,22 @@
-// Initialize Firebase
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDM3cs-CkpXNSXDiFYjuiNzdiGLMXZKZ5o",
   authDomain: "holidaysnaps-af8d3.firebaseapp.com",
   projectId: "holidaysnaps-af8d3",
-  storageBucket: "holidaysnaps-af8d3.appspot.com",
+  storageBucket: "holidaysnaps-af8d3.appspot.com", // ← IMPORTANT: .appspot.com not .firebasestorage.app
   messagingSenderId: "438127405798",
   appId: "1:438127405798:web:96fcaf79348e0a1e586927",
   measurementId: "G-BXC6W0GZ08"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export all Firebase services
+export { app, auth, db };
