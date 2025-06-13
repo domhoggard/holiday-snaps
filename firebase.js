f378003093677df0b3da1c00c633a4bd7c93a119
domhoggard <domhoggard@me.com>
2025-06-13 12:57:34 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-storage.js";
 
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDM3cs-CkpXNSXDiFYjuiNzdiGLMXZKZ5o",
   authDomain: "holidaysnaps-af8d3.firebaseapp.com",
   projectId: "holidaysnaps-af8d3",
-  storageBucket: "holidaysnaps-af8d3.appspot.com",
+  storageBucket: "holidaysnaps-af8d3.firebasestorage.app",
   messagingSenderId: "438127405798",
   appId: "1:438127405798:web:96fcaf79348e0a1e586927",
   measurementId: "G-BXC6W0GZ08"
@@ -18,4 +18,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { app, auth, db, storage, signOut };
+export { auth, db, storage };
--- login.js
+++ login.js
@@ -1,16 +1,1 @@
-import { auth } from './firebase.js';
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
-
-document.getElementById('login-form').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    alert("Login successful!");
-    window.location.href = "profile.html";
-  } catch (error) {
-    alert("Login failed: " + error.message);
-  }
-});
+// JS functionality for login page will be added next.
